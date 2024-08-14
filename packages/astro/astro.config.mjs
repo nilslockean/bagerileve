c40648797f962c9cb5764d45bfc8189e3dd1c9fe
Nils Lockean <nilslockean@gmail.com>
2024-08-15 00:03:43 +0200

    Install Prettier plugin

--- packages/astro/astro.config.mjs
+++ packages/astro/astro.config.mjs
@@ -1,22 +1,26 @@
-import { defineConfig } from 'astro/config';
-import svelte from '@astrojs/svelte';
-import tailwind from '@astrojs/tailwind';
-import sanity from '@sanity/astro';
-import { loadEnv } from 'vite';
+import { defineConfig } from "astro/config";
+import svelte from "@astrojs/svelte";
+import tailwind from "@astrojs/tailwind";
+import sanity from "@sanity/astro";
+import { loadEnv } from "vite";
 
 // https://docs.astro.build/en/guides/configuring-astro/#environment-variables
-const { SANITY_PROJECT_ID, SANITY_DATASET } = loadEnv(process.env.NODE_ENV, process.cwd(), '');
+const { SANITY_PROJECT_ID, SANITY_DATASET } = loadEnv(
+  process.env.NODE_ENV,
+  process.cwd(),
+  "",
+);
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		svelte(),
-		tailwind(),
-		sanity({
-			projectId: SANITY_PROJECT_ID,
-			dataset: SANITY_DATASET,
-			apiVersion: '2024-01-21',
-			useCdn: false
-		})
-	]
+  integrations: [
+    svelte(),
+    tailwind(),
+    sanity({
+      projectId: SANITY_PROJECT_ID,
+      dataset: SANITY_DATASET,
+      apiVersion: "2024-01-21",
+      useCdn: false,
+    }),
+  ],
 });
