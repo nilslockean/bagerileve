9ea17f57b15e0b37f0523be9d115aae0949bf675
Nils Lockean <nilslockean@gmail.com>
2024-08-25 16:48:03 +0200

    Test env

--- packages/astro/src/pages/api/events.json.ts
+++ packages/astro/src/pages/api/events.json.ts
@@ -1,8 +1,19 @@
 export async function GET() {
-  return new Response(JSON.stringify({ now: Date.now() }), {
-    status: 200,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  return new Response(
+    JSON.stringify(
+      {
+        now: Date.now(),
+        meta: import.meta.env.FIENTA_API_KEY || "",
+        process: process.env.FIENTA_API_KEY || "",
+      },
+      null,
+      2
+    ),
+    {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 }
