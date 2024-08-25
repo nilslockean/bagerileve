12bd5f36fc83e0e03d98f202166926348092e321
Nils Lockean <nilslockean@gmail.com>
2024-08-25 17:51:18 +0200

    Try locals again

--- packages/astro/src/pages/api/events.json.ts
+++ packages/astro/src/pages/api/events.json.ts
@@ -1,12 +1,15 @@
+import type { APIContext } from "astro";
+
 export const prerender = false;
 
-export async function GET() {
+export async function GET(context: APIContext) {
   return new Response(
     JSON.stringify(
       {
         now: Date.now(),
         meta: import.meta.env.FIENTA_API_KEY || "",
         process: process.env.FIENTA_API_KEY || "",
+        locals: context.locals.runtime.env.FIENTA_API_KEY || "",
       },
       null,
       2
