b201be20dba02e73101d6b0749561cbea04f2edc
Nils Lockean <nilslockean@gmail.com>
2024-08-27 22:32:36 +0200

    Load env manually in sanityAsset function

--- packages/astro/src/lib/sanityAsset.ts
+++ packages/astro/src/lib/sanityAsset.ts
@@ -1,15 +1,7 @@
-import { loadEnv } from "vite";
-
-const { SANITY_PROJECT_ID, SANITY_DATASET } = loadEnv(
-  process.env.NODE_ENV || "production",
-  process.cwd(),
-  ""
-);
-
 export function sanityAsset(
   filename: string,
-  projectId = SANITY_PROJECT_ID,
-  dataset = SANITY_DATASET
+  projectId: string,
+  dataset: string
 ): string {
   return `https://cdn.sanity.io/files/${projectId}/${dataset}/${filename}`;
 }
