0cb0e0a494963bd716c40e7eb158ed38d93ba6c9
Nils Lockean <nilslockean@gmail.com>
2024-08-30 17:28:42 +0200

    refactor: Use Wrangler dev preview URL in e2e tests

--- packages/astro/playwright.config.ts
+++ packages/astro/playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://localhost:4321",
+    baseURL: "http://localhost:8788",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -72,7 +72,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "npm run preview",
-    url: "http://localhost:4321/",
+    url: "http://localhost:8788/",
     timeout: 20 * 1000,
     reuseExistingServer: !process.env.CI,
   },
