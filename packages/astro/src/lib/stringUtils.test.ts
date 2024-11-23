494a4aff6b1dab1a2e5bb7fc656a56190de45c9d
Nils Lockean <nilslockean@gmail.com>
2024-11-23 17:45:10 +0100

    feature/course pages (#25)
    * Add button component
    
    * Add basic course pages
    
    * Fix dev script in root package.json
    
    * feat: Add CourseList component and refactor event page to use it
    
    * style: Add list styles to base.css and remove inline styles from course page
    
    * fix: Update prettyCourseDates to include year in formatted output
    
    * feat: Add Hero component and refactor index and course pages to utilize it
    
    * refactor: Make Accordion component id prop optional and update usage in pages
    
    * feat: Display past courses in accordion
    
    ---------
    
    Co-authored-by: Nils Lockean <nils.lockean@texthelp.com>

--- packages/astro/src/lib/stringUtils.test.ts
+++ packages/astro/src/lib/stringUtils.test.ts
@@ -6,7 +6,7 @@ describe("prettyCourseDates", () => {
     const start = new Date("2024-11-04T16:30:00+02:00");
     const end = new Date("2024-11-05T17:00:00+02:00");
     expect(prettyCourseDates(start, end)).toBe(
-      "Måndag 4 november - tisdag 5 november"
+      "Måndag 4 november - tisdag 5 november 2024"
     );
   });
 
@@ -14,26 +14,28 @@ describe("prettyCourseDates", () => {
     const start = new Date("2024-11-04T16:30:00+02:00");
     const end = start;
 
-    expect(prettyCourseDates(start, end)).toBe("Måndag 4 november kl. 16:30");
+    expect(prettyCourseDates(start, end)).toBe(
+      "Måndag 4 november 2024 kl. 16:30"
+    );
   });
 
   test("no end date", () => {
     const start = new Date("2024-11-04T16:30:00+02:00");
 
-    expect(prettyCourseDates(start)).toBe("Måndag 4 november kl. 16:30");
+    expect(prettyCourseDates(start)).toBe("Måndag 4 november 2024 kl. 16:30");
   });
 
   test("starts and ends on same day but different times", () => {
     const start = new Date("2024-11-04T16:30:00+02:00");
     const end = new Date("2024-11-04T17:00:00+02:00");
     expect(prettyCourseDates(start, end)).toBe(
-      "Måndag 4 november kl. 16:30 - 17:00"
+      "Måndag 4 november 2024 kl. 16:30 - 17:00"
     );
   });
 
   test("end date set but not time", () => {
     const start = new Date("2024-03-27T12:00:00+02:00");
     const end = new Date("2024-03-27T23:59:59+02:00");
-    expect(prettyCourseDates(start, end)).toBe("Onsdag 27 mars kl. 12:00");
+    expect(prettyCourseDates(start, end)).toBe("Onsdag 27 mars 2024 kl. 12:00");
   });
 });
