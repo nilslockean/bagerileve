45bf241344a8e18c8796e71ad4f7166bf1425e42
Nils Lockean <nilslockean@gmail.com>
2024-11-24 22:57:55 +0100

    chore/fetch single event (#26)
    * refactor: Update course schemas and response handling for Fienta API
    
    * fix: Correct import path for Course schema in CourseList component
    
    * refactor: Remove unused variables
    
    * feature: Add function to fetch a single course
    
    * feature: Implement FientaAPI class for course data fetching and management
    
    ---------
    
    Co-authored-by: Nils Lockean <nils.lockean@texthelp.com>

---
+++ packages/astro/src/lib/schemas/FientaEventDetails.ts
@@ -0,0 +1,46 @@
+import { z } from "zod";
+
+export const FientaEventDetailsSchema = z.object({
+  id: z.number(),
+  // organizer_id: z.number(),
+  starts_at: z.string(),
+  ends_at: z.string(),
+  sale_status: z.enum(["onSale", "salesEnded", "salesNotStarted", "soldOut"]),
+  is_published: z.boolean(),
+  is_public: z.boolean(),
+  image_url: z
+    .string()
+    .url()
+    .transform((url) => {
+      return url.endsWith(".jpg") ? url : undefined;
+    }),
+  // accent_color: z.string(),
+  url: z.string().url(),
+  buy_tickets_url: z.string().url(),
+  translations: z.object({
+    sv: z.object({
+      title: z.string(),
+      description: z.string({
+        description: "HTML",
+      }),
+      duration_string: z.string(),
+      notes_about_time: z.string().nullable(),
+      // venue: z.string(),
+      // online_location: z.string().nullable(),
+      // address: z.object({
+      // 	street: z.string(),
+      // 	city: z.string(),
+      // 	county: z.string(),
+      // 	postal_code: z.string(),
+      // 	country_code: z.string()
+      // }),
+      // organizer: z.object({
+      // 	name: z.string(),
+      // 	phone: z.string(),
+      // 	email: z.string().email()
+      // })
+    }),
+  }),
+});
+
+export type FientaEventDetails = z.infer<typeof FientaEventDetailsSchema>;
--- packages/astro/src/lib/schemas/FientaResponseSchema.ts
+++ packages/astro/src/lib/schemas/FientaResponseSchema.ts
@@ -1,82 +1,36 @@
 import { z } from "zod";
 
-const _FientaSuccessResponseSchema = z.object({
-  success: z.object({
-    code: z.number(),
-    // user_message: z.string(),
-    // internal_message: z.string()
-  }),
-  time: z.object({
-    timestamp: z.number(),
-    // date: z.string(),
-    // time: z.string(),
-    full_datetime: z.string(),
-    // timezone: z.string(),
-    // timezone_short: z.string(),
-    // gmt: z.string()
-  }),
-  data: z.array(
-    z.object({
-      id: z.number(),
-      // organizer_id: z.number(),
-      starts_at: z.string(),
-      ends_at: z.string(),
-      sale_status: z.enum([
-        "onSale",
-        "salesEnded",
-        "salesNotStarted",
-        "soldOut",
-      ]),
-      is_published: z.boolean(),
-      is_public: z.boolean(),
-      image_url: z
-        .string()
-        .url()
-        .transform((url) => {
-          return url.endsWith(".jpg") ? url : undefined;
-        }),
-      // accent_color: z.string(),
-      url: z.string().url(),
-      buy_tickets_url: z.string().url(),
-      translations: z.object({
-        sv: z.object({
-          title: z.string(),
-          description: z.string({
-            description: "HTML",
-          }),
-          duration_string: z.string(),
-          notes_about_time: z.string().nullable(),
-          // venue: z.string(),
-          // online_location: z.string().nullable(),
-          // address: z.object({
-          // 	street: z.string(),
-          // 	city: z.string(),
-          // 	county: z.string(),
-          // 	postal_code: z.string(),
-          // 	country_code: z.string()
-          // }),
-          // organizer: z.object({
-          // 	name: z.string(),
-          // 	phone: z.string(),
-          // 	email: z.string().email()
-          // })
-        }),
-      }),
-    })
-  ),
-});
-
-const _FientaErrorResponseSchema = z.object({
+export const ErrorResponseSchema = z.object({
   errors: z.array(
     z.object({
-      code: z.string(),
+      code: z.union([z.number(), z.string()]),
       user_message: z.string(),
       internal_message: z.string(),
     })
   ),
 });
+export type ErrorResponse = z.infer<typeof ErrorResponseSchema>;
 
-export const FientaResponseSchema = z.union([
-  _FientaSuccessResponseSchema,
-  _FientaErrorResponseSchema,
-]);
+function composeSuccessSchema<T extends z.ZodTypeAny>(dataSchema: T) {
+  return z.object({
+    success: z.object({
+      code: z.number(),
+      // user_message: z.string(),
+      // internal_message: z.string()
+    }),
+    time: z.object({
+      timestamp: z.number(),
+      // date: z.string(),
+      // time: z.string(),
+      full_datetime: z.string(),
+      // timezone: z.string(),
+      // timezone_short: z.string(),
+      // gmt: z.string()
+    }),
+    data: dataSchema,
+  });
+}
+
+export function composeResponseSchema<T extends z.ZodTypeAny>(dataSchema: T) {
+  return z.union([composeSuccessSchema(dataSchema), ErrorResponseSchema]);
+}
