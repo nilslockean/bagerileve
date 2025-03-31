30b045cd69d3609bf00c0ff3c94b5b033fa5f7a1
Nils Lockean <nils.lockean@texthelp.com>
2025-03-31 10:02:06 +0200

    feature: add product schema to Sanity and update package.json scripts

--- packages/sanity/schemas/index.ts
+++ packages/sanity/schemas/index.ts
@@ -1,5 +1,6 @@
 import faq from './faq'
 import openingHours from './opening-hours'
 import orderTerms from './orderTerms'
+import product from './product'
 
-export const schemaTypes = [openingHours, faq, orderTerms]
+export const schemaTypes = [openingHours, faq, orderTerms, product]
