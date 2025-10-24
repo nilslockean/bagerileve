69104e1f31552a5201932a243f6ddd78d0086929
Nils Lockean <nils.lockean@texthelp.com>
2025-10-24 21:11:35 +0200

    refactor: remove shared package and update Astro configuration
    - Deleted the shared package, including its environment variables and TypeScript configuration.
    - Updated Astro configuration to directly use the SANITY_PROJECT_ID and dataset values.
    - Adjusted ProductDetail component to remove unused imports related to pickup date configuration.
    - Enhanced Sanity configuration to support both production and staging environments with distinct settings.

--- packages/sanity/sanity.config.ts
+++ packages/sanity/sanity.config.ts
@@ -4,16 +4,29 @@ import {schemaTypes} from './schemas'
 import {svSELocale} from '@sanity/locale-sv-se'
 import {visionTool} from '@sanity/vision'
 
-export default defineConfig({
-  name: 'default',
-  title: 'Bageri Leve',
-
-  projectId: 'mz20cm4o',
-  dataset: 'production',
-
-  plugins: [structureTool(), svSELocale(), visionTool()],
-
-  schema: {
-    types: schemaTypes,
+export default defineConfig([
+  {
+    projectId: 'mz20cm4o',
+    dataset: 'production',
+    name: 'default',
+    basePath: '/production',
+    title: 'Bageri Leve',
+    subtitle: 'production',
+    plugins: [structureTool(), svSELocale(), visionTool()],
+    schema: {
+      types: schemaTypes,
+    },
   },
-})
+  {
+    projectId: 'mz20cm4o',
+    dataset: 'preview',
+    name: 'staging-workspace',
+    basePath: '/staging',
+    title: '[TEST] Bageri Leve',
+    subtitle: 'staging',
+    plugins: [structureTool(), svSELocale(), visionTool()],
+    schema: {
+      types: schemaTypes,
+    },
+  },
+])
