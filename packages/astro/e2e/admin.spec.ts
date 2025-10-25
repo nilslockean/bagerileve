edd057701e11fd567670466111bf81f54701034b
Nils Lockean <nils.lockean@texthelp.com>
2025-10-25 15:45:05 +0200

    fix: update redirect URL in admin.spec.ts for Sanity Studio
    - Changed the expected URL in the admin test from "https://leve.sanity.studio/" to "https://leve.sanity.studio/production" to reflect the correct production environment.

--- packages/astro/e2e/admin.spec.ts
+++ packages/astro/e2e/admin.spec.ts
@@ -5,6 +5,6 @@ test.describe("/admin", () => {
   test("should redirect to Sanity Studio", async ({ page }) => {
     await page.goto("/admin");
     const url = page.url();
-    expect(url).toBe("https://leve.sanity.studio/");
+    expect(url).toBe("https://leve.sanity.studio/production");
   });
 });
